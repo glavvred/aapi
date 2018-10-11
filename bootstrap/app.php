f899fc2918ec8b8d58306d69f19ce0e719aaa75e
glavvred <glavvred@wand.ru>
2018-10-11 17:10:56 +0300

    +CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,9 +59,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 $app->routeMiddleware([
     'jwt.auth' => App\Http\Middleware\JwtMiddleware::class,
